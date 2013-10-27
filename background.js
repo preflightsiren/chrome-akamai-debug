9f82d6becd1a922da220526f90620d842543e38a
Sebastian Cole <sebastian.paris@gmail.com>
2013-10-27 23:48:41 +1100

    Initial Working version of Akamai Headers
    manifest.json and background.js
    Appends akamai debug headers to requests that contain a http Pragma
    header.
    Adds a new http header if no existing header is found

---
+++ background.js
@@ -0,0 +1,21 @@
+var debug_headers = "akamai-x-get-client-ip, akamai-x-cache-on, akamai-x-cache-remote-on, akamai-x-check-cacheable, akamai-x-get-cache-key, akamai-x-get-extracted-values, akamai-x-get-nonces, akamai-x-get-ssl-client-session-id, akamai-x-get-true-cache-key, akamai-x-serial-no"
+var akamai_header = { "name":"Pragma", "value":debug_headers}
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    console.log("Akamai headers added to: " + details.url);
+    var pragma_exists = false
+    for (var i = 0; i < details.requestHeaders.length; ++i) {
+      if (details.requestHeaders[i].name === 'Pragma') {
+        details.requestHeaders[i].value = details.requestHeaders[i].value.concat(", ",debug_headers)
+        pragma_exists = true
+        break;
+      }
+    }
+    if(!pragma_exists) {
+      details.requestHeaders.push(akamai_header)
+    }
+    return {requestHeaders: details.requestHeaders};
+  },
+  {urls: ["<all_urls>"]},
+  ["blocking", "requestHeaders"]);
